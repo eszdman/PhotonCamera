84a51beaffc2a544210bdfa8ecf8f600691f15ee
assasinfil <nfa57643@gmail.com>
2020-05-25 16:38:26 +0300

    Save settings fix

--- app/src/main/java/com/eszdman/photoncamera/MainActivity.java
+++ app/src/main/java/com/eszdman/photoncamera/MainActivity.java
@@ -3,6 +3,7 @@ package com.eszdman.photoncamera;
 import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
@@ -13,21 +14,26 @@ import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
     public static MainActivity act;
+
     static {
         if (!OpenCVLoader.initDebug()) {
             // Handle initialization error
         }
     }
+
     @Override
-    public void onBackPressed()
-    {
-        if(Camera2Api.context.mState != 5)  {super.onBackPressed(); return;} // optional depending on your needs
+    public void onPause() {
+        super.onPause();
+        if (Camera2Api.context.mState != 5) {
+            return;
+        } // optional depending on your needs
         else {//inst.closeCamera();
-        //inst.onPause();
-        //Camera2Api.context.onPause();
-        try {
-            Settings.instance.getSettings();
-        }catch (Exception e){}
+            //inst.onPause();
+            //Camera2Api.context.onPause();
+            try {
+                Settings.instance.getSettings();
+            } catch (Exception ignored) {
+            }
         }
         setContentView(R.layout.activity_camera);
         Intent intent = this.getIntent();
@@ -35,13 +41,14 @@ public class MainActivity extends AppCompatActivity {
         this.startActivity(intent);
         Animatoo.animateShrink(this);
     }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Wrapper wp = new Wrapper();
         Photo photo = new Photo();
         act = this;
-        Permissions.RequestPermissions(MainActivity.this, 2,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA});
+        Permissions.RequestPermissions(MainActivity.this, 2, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA});
         Settings settings = new Settings();
         setContentView(R.layout.activity_camera);
         Camera2Api.context = Camera2Api.newInstance();
