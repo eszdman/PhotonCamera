96cec048c6e084cde6197dcd51e6f7ea20ecfa0b
eszdman <eszdman2015@gmail.com>
2020-05-28 02:36:52 +0300

    Updated code/ui

--- app/src/main/java/com/eszdman/photoncamera/api/CameraReflectionApi.java
+++ app/src/main/java/com/eszdman/photoncamera/api/CameraReflectionApi.java
@@ -1,9 +1,14 @@
 package com.eszdman.photoncamera.api;
 
+import android.Manifest;
+import android.app.Application;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CaptureResult;
+import android.util.Log;
 
 import com.eszdman.photoncamera.ui.CameraFragment;
+
+import org.chickenhook.restrictionbypass.RestrictionBypass;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -13,10 +18,11 @@ public class CameraReflectionApi {
 
     public static <T> void set(CameraCharacteristics.Key<T> key, T value) {
         try {
-            Field CameraMetadataNativeField = CameraCharacteristics.class.getDeclaredField("mProperties");
+            //RestrictionBypass.getDeclaredField(CameraCharacteristics.class, "mProperties");
+            Field CameraMetadataNativeField = RestrictionBypass.getDeclaredField(CameraCharacteristics.class, "mProperties");//= CameraCharacteristics.class.getDeclaredField("mProperties");
             CameraMetadataNativeField.setAccessible(true);
             Object CameraMetadataNative = CameraMetadataNativeField.get(CameraFragment.mCameraCharacteristics);//Ur camera Characteristics
-            Method set = CameraMetadataNative.getClass().getDeclaredMethod("set", CameraCharacteristics.Key.class, Object.class);
+            Method set = RestrictionBypass.getDeclaredMethod(CameraMetadataNative.getClass(),"set",CameraCharacteristics.Key.class, Object.class);//CameraMetadataNative.getClass().getDeclaredMethod("set", CameraCharacteristics.Key.class, Object.class);
             set.setAccessible(true);
             set.invoke(CameraMetadataNative, key, value);
         } catch (Exception e) {
@@ -26,15 +32,34 @@ public class CameraReflectionApi {
 
     public static <T> void set(CaptureResult.Key<T> key, T value) {
         try {
-            Field CameraMetadataNativeField = CaptureResult.class.getDeclaredField("mResults");
+            //Field CameraMetadataNativeField = CaptureResult.class.getDeclaredField("mResults");
+            Field CameraMetadataNativeField = RestrictionBypass.getDeclaredField(CaptureResult.class,"mResults");
             CameraMetadataNativeField.setAccessible(true);
             Object CameraMetadataNative = CameraMetadataNativeField.get(CameraFragment.mCaptureResult);//Ur camera CaptureResult
-            Method set = CameraMetadataNative.getClass().getDeclaredMethod("set", CaptureResult.Key.class, Object.class);
+            Method set = RestrictionBypass.getDeclaredMethod(CameraMetadataNative.getClass(),"set", CaptureResult.Key.class, Object.class);//CameraMetadataNative.getClass().getDeclaredMethod("set", CaptureResult.Key.class, Object.class);
             set.setAccessible(true);
             set.invoke(CameraMetadataNative, key, value);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    public static void setAuxOn(){
+        Log.d(TAG,"Setting Aux ON");
+        try {
+            /*Class actThread = Class.forName("android.app.ActivityThread");
+            Method getCur = RestrictionBypass.getDeclaredMethod(actThread,"currentActivityThread");
+            getCur.setAccessible(true);
+            Object curThread = getCur.invoke(null);
+            Field isSystem = RestrictionBypass.getDeclaredField(curThread.getClass(),"mSystemThread");
+            isSystem.set(curThread,true);*/
+            Class SystemProperties = Class.forName("android.os.SystemProperties");
+            Method set = RestrictionBypass.getDeclaredMethod(SystemProperties,"native_set",String.class,String.class);
+            set.setAccessible(true);
+            //set.invoke(null,"camera.aux.packagelist", "com.eszdman.photoncamera");//ActivityThread.currentPackageName()
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        Log.d(TAG,"Setting Aux done!");
     }
 
     public static void setVERBOSE(boolean in) {
