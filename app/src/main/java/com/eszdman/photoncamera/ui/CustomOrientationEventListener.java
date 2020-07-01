a8477bfc30f069d7f5dcaa1bcd2845c7e133ab8a
Urnyx05 <38892752+urnyx05@users.noreply.github.com>
2020-07-01 11:43:37 +0200

    Added rotation of buttons in viewfinder

---
+++ app/src/main/java/com/eszdman/photoncamera/ui/CustomOrientationEventListener.java
@@ -0,0 +1,59 @@
+package com.eszdman.photoncamera.ui;
+
+import android.content.Context;
+import android.provider.Settings;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+
+public abstract class CustomOrientationEventListener  extends OrientationEventListener {
+
+
+    private static final String TAG = "CustomOrientationEvent";
+    private int prevOrientation = OrientationEventListener.ORIENTATION_UNKNOWN;
+    private Context context;
+    private final int ROTATION_O    = 1;
+    private final int ROTATION_90   = 2;
+    private final int ROTATION_180  = 3;
+    private final int ROTATION_270  = 4;
+    private int rotation = 0;
+
+    public CustomOrientationEventListener(Context context) {
+        super(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onOrientationChanged(int orientation) {
+
+        if (android.provider.Settings.System.getInt(context.getContentResolver(), Settings.System.ACCELEROMETER_ROTATION, 0) == 0) // 0 = Auto Rotate Disabled
+            return;
+        int currentOrientation = OrientationEventListener.ORIENTATION_UNKNOWN;
+        if (orientation >= 340 || orientation < 20 && rotation != ROTATION_O) {
+            currentOrientation = Surface.ROTATION_0;
+            rotation = ROTATION_O;
+
+        } else if (orientation >= 70 && orientation < 110 && rotation != ROTATION_90) {
+            currentOrientation = Surface.ROTATION_90;
+            rotation = ROTATION_90;
+
+        } else if (orientation >= 160 && orientation < 200 && rotation != ROTATION_180) {
+            currentOrientation = Surface.ROTATION_180;
+            rotation = ROTATION_180;
+
+        } else if (orientation >= 250 && orientation < 290 && rotation != ROTATION_270) {
+            currentOrientation = Surface.ROTATION_270;
+            rotation = ROTATION_270;
+        }
+
+        if (prevOrientation != currentOrientation && orientation != OrientationEventListener.ORIENTATION_UNKNOWN) {
+            prevOrientation = currentOrientation;
+            if (currentOrientation != OrientationEventListener.ORIENTATION_UNKNOWN) {
+                onSimpleOrientationChanged(rotation);
+            }
+        }
+    }
+
+    public abstract void onSimpleOrientationChanged(int orientation);
+
+
+}
--- app/src/main/java/com/eszdman/photoncamera/ui/MainActivity.java
+++ app/src/main/java/com/eszdman/photoncamera/ui/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,8 +22,17 @@ import com.eszdman.photoncamera.api.Permissions;
 
 import org.opencv.android.OpenCVLoader;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private CustomOrientationEventListener customOrientationEventListener;
+    final int ROTATION_O    = 1;
+    final int ROTATION_90   = 2;
+    final int ROTATION_180  = 3;
+    final int ROTATION_270  = 4;
+
     public static MainActivity act;
     static {
         if (!OpenCVLoader.initDebug()) {
@@ -58,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         act = this;
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Interface inter = new Interface(this);
 //        Wrapper.Test();
         Permissions.RequestPermissions(this, 2, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA});
@@ -70,5 +82,95 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, CameraFragment.context)
                     .commit();
         }
+
+        customOrientationEventListener = new
+                CustomOrientationEventListener(getBaseContext()) {
+                    @Override
+                    public void onSimpleOrientationChanged(int orientation) {
+                        ToggleButton stacking = findViewById(R.id.stacking);
+                        Button settings = findViewById(R.id.settings);
+                        ImageButton flipcam = findViewById(R.id.flip_camera);
+                        CircleImageView gallery = findViewById(R.id.ImageOut);
+                        ImageView expText = findViewById(R.id.expText);
+                        TextView expValue = findViewById(R.id.expValue);
+                        TextView isoText = findViewById(R.id.isoText);
+                        TextView isoValue = findViewById(R.id.isoValue);
+                        ImageView focusText = findViewById(R.id.focusText);
+                        TextView focusValue = findViewById(R.id.focusValue);
+                        switch(orientation){
+                            case ROTATION_O:
+                                //rotate as on portrait
+                                stacking.animate().rotation(0).setDuration(350).start();
+                                settings.animate().rotation(0).setDuration(350).start();
+                                flipcam.animate().rotation(0).setDuration(350).start();
+                                gallery.animate().rotation(0).setDuration(350).start();
+                                expText.animate().rotation(0).setDuration(350).start();
+                                expValue.animate().rotation(0).setDuration(350).start();
+                                isoText.animate().rotation(0).setDuration(350).start();
+                                isoValue.animate().rotation(0).setDuration(350).start();
+                                focusText.animate().rotation(0).setDuration(350).start();
+                                focusValue.animate().rotation(0).setDuration(350).start();
+                                break;
+                            case ROTATION_90:
+                                //rotate as left on top
+                                stacking.animate().rotation(-90).setDuration(350).start();
+                                settings.animate().rotation(-90).setDuration(350).start();
+                                flipcam.animate().rotation(-90).setDuration(350).start();
+                                gallery.animate().rotation(-90).setDuration(350).start();
+                                expText.animate().rotation(-90).setDuration(350).start();
+                                expValue.animate().rotation(-90).setDuration(350).start();
+                                isoText.animate().rotation(-90).setDuration(350).start();
+                                isoValue.animate().rotation(-90).setDuration(350).start();
+                                focusText.animate().rotation(-90).setDuration(350).start();
+                                focusValue.animate().rotation(-90).setDuration(350).start();
+                                break;
+                            case ROTATION_270:
+                                //rotate as right on top
+                                stacking.animate().rotation(90).setDuration(350).start();
+                                settings.animate().rotation(90).setDuration(350).start();
+                                flipcam.animate().rotation(90).setDuration(350).start();
+                                gallery.animate().rotation(90).setDuration(350).start();
+                                expText.animate().rotation(90).setDuration(350).start();
+                                expValue.animate().rotation(90).setDuration(350).start();
+                                isoText.animate().rotation(90).setDuration(350).start();
+                                isoValue.animate().rotation(90).setDuration(350).start();
+                                focusText.animate().rotation(90).setDuration(350).start();
+                                focusValue.animate().rotation(90).setDuration(350).start();
+                                break;
+                            case ROTATION_180:
+                                //rotate as upside down
+                                stacking.animate().rotation(180).setDuration(350).start();
+                                settings.animate().rotation(180).setDuration(350).start();
+                                flipcam.animate().rotation(180).setDuration(350).start();
+                                gallery.animate().rotation(180).setDuration(350).start();
+                                expText.animate().rotation(180).setDuration(350).start();
+                                expValue.animate().rotation(180).setDuration(350).start();
+                                isoText.animate().rotation(180).setDuration(350).start();
+                                isoValue.animate().rotation(180).setDuration(350).start();
+                                focusText.animate().rotation(180).setDuration(350).start();
+                                focusValue.animate().rotation(180).setDuration(350).start();
+                                break;
+
+                        }
+                    }
+                };
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        customOrientationEventListener.enable();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        customOrientationEventListener.disable();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        customOrientationEventListener.disable();
     }
 }
