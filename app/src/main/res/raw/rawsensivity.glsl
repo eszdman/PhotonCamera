d08b62393684364e2501061e65182138a132349c
eszdman <eszdman2015@gmail.com>
2020-07-05 04:12:43 +0300

    Added rawsensivity.glsl

---
+++ app/src/main/res/raw/rawsensivity.glsl
@@ -0,0 +1,19 @@
+#version 300 es
+precision mediump float;
+precision mediump usampler2D;
+uniform usampler2D RawBuffer;
+uniform usampler2D HotPixelMap;
+ivec2 HotPixelMapSize;
+uniform int yOffset;
+float PostRawSensivity;
+
+out uint Output;
+
+void main() {
+    ivec2 xy = ivec2(gl_FragCoord.xy);
+    xy+=ivec2(0,yOffset);
+    float rawIn = float(texelFetch(RawBuffer, (xy), 0).x);
+    rawIn*=PostRawSensivity;
+    rawIn = clamp(rawIn,0.0,65535.0);
+    Output = uint(rawIn);
+}
