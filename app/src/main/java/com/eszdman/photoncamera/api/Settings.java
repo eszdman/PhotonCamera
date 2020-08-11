44b63574db75f45cd3215289d42a0c659f98f8ec
eszdman <eszdman2015@gmail.com>
2020-08-12 02:17:39 +0300

    Reduced sharpening

--- app/src/main/java/com/eszdman/photoncamera/api/Settings.java
+++ app/src/main/java/com/eszdman/photoncamera/api/Settings.java
@@ -45,7 +45,7 @@ public class Settings {
     public boolean hdrx = true;
     public boolean hdrxNR = true;
     public double saturation = 1.2;
-    public double sharpness = 1.5;
+    public double sharpness = 0.7;
     public double contrastMpy = 1.0;
     public int contrastConst = 0;
     public double compressor = 1.6;
