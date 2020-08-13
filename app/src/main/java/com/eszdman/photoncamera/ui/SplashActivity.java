b9d6ba802ba6231562fddf3bd4055d3380fdf6a7
Urnyx05 <38892752+urnyx05@users.noreply.github.com>
2020-08-13 12:30:06 +0200

    Added splash screen

---
+++ app/src/main/java/com/eszdman/photoncamera/ui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.eszdman.photoncamera.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+        finish();
+    }
+}
