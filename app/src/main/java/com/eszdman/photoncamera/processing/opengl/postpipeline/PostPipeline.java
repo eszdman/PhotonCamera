10e1b90693056d9f5a2568dab001fbdd694bdba8
Urnyx05 <38892752+urnyx05@users.noreply.github.com>
2020-10-02 23:52:11 +0200

    Reduced apk size

--- app/src/main/java/com/eszdman/photoncamera/processing/opengl/postpipeline/PostPipeline.java
+++ app/src/main/java/com/eszdman/photoncamera/processing/opengl/postpipeline/PostPipeline.java
@@ -105,6 +105,7 @@ public class PostPipeline extends GLBasePipeline {
         add(new Initial(R.raw.initial,"Initial"));
         //add(new AWB(0,"AWB"));
         add(new GlobalTonemaping(0,"GlobalTonemap"));
+        //fix yellow AWB - Urnyx05
         if(PhotonCamera.getSettings().hdrxNR) {
             add(new NoiseDetection(R.raw.noisedetection44,"NoiseDetection"));
             add(new NoiseMap(R.raw.gaussdown44,"GaussDownMap"));
