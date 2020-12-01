0292a5e8b4c515a9b597e4fd898ec8a43af1e1c1
Vibhor Srivastava <69432426+vibhorSrv@users.noreply.github.com>
2020-12-01 20:12:35 +0530

    Better null checks

--- app/src/main/java/com/eszdman/photoncamera/gallery/viewmodel/ExifDialogViewModel.java
+++ app/src/main/java/com/eszdman/photoncamera/gallery/viewmodel/ExifDialogViewModel.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.util.Log;
 import android.util.Rational;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,7 +21,6 @@ import rapid.decoder.BitmapDecoder;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -29,6 +29,7 @@ import java.util.Locale;
  * The View Model class which updates the {@link ExifDialogModel}
  */
 public class ExifDialogViewModel extends ViewModel {
+    private static final String TAG = ExifDialogViewModel.class.getSimpleName();
     private final ExifDialogModel exifDialogModel;
 
     public ExifDialogViewModel() {
@@ -42,7 +43,7 @@ public class ExifDialogViewModel extends ViewModel {
     /**
      * Updates the ExifDialogModel using exif attributes stored in the Image File
      *
-     * @param imageFile      the image imageFile whose exif data is to be read
+     * @param imageFile the image imageFile whose exif data is to be read
      * @param histogram object of {@link Histogram}
      */
     public void updateModel(File imageFile, Histogram histogram) {
@@ -65,10 +66,10 @@ public class ExifDialogViewModel extends ViewModel {
 //        String attr_35mmfocal = exifInterface.getAttribute(ExifInterface.TAG_FOCAL_LENGTH_IN_35MM_FILM);
 //        Log.d("attr_35mmfocal", "fetched attr_35mmfocal = " + attr_35mmfocal);
 
-        String exposure = (Utilities.formatExposureTime(Double.parseDouble(attr_exp == null ? "-1" : attr_exp)));
+        String exposure = (Utilities.formatExposureTime(Double.parseDouble(attr_exp == null ? "NaN" : attr_exp)));
         String resolution_mp = (String.format(Locale.US, "%.1f",
-                Double.parseDouble((attr_width == null ? "-1" : attr_width))
-                        * Double.parseDouble((attr_length == null ? "-1" : attr_length)) / 1E6) + " MP");
+                Double.parseDouble((attr_width == null ? "NaN" : attr_width))
+                        * Double.parseDouble((attr_length == null ? "NaN" : attr_length)) / 1E6) + " MP");
 
         exifDialogModel.setTitle(imageFile.getAbsolutePath());
         exifDialogModel.setRes(attr_length + "x" + attr_width);
@@ -77,7 +78,7 @@ public class ExifDialogViewModel extends ViewModel {
         exifDialogModel.setExposure(exposure + "s");
         exifDialogModel.setIso("ISO" + attr_iso);
         exifDialogModel.setFnum("\u0192/" + attr_fnum);
-        exifDialogModel.setFocal(Rational.parseRational(attr_focal).doubleValue() + "mm");
+        exifDialogModel.setFocal(Rational.parseRational(attr_focal == null ? "NaN" : attr_focal).doubleValue() + "mm");
         exifDialogModel.setFile_size((FileUtils.byteCountToDisplaySize((int) imageFile.length())));
         exifDialogModel.setRes_mp(resolution_mp);
         updateHistogramView(imageFile, histogram);
@@ -89,16 +90,20 @@ public class ExifDialogViewModel extends ViewModel {
      * check for more detail {@link com.eszdman.photoncamera.gallery.binding.CustomBinding#updateHistogram(ViewGroup, ExifDialogModel)}
      */
     private void updateHistogramView(File imageFile, Histogram histogram) {
-        Handler handler = new Handler(Looper.myLooper(), msg -> {
+        Handler handler = new Handler(Looper.getMainLooper(), msg -> {
             exifDialogModel.setHistogram((View) msg.obj); //setting histogram view to model
             return true;
         });
         Thread th = new Thread(() -> {
             Bitmap preview = BitmapDecoder.from(Uri.fromFile(imageFile)).scaleBy(0.1f).decode();
-            histogram.Analyze(preview);
-            Message msg = new Message();
-            msg.obj = histogram;
-            handler.sendMessage(msg);
+            if (preview != null) {
+                histogram.Analyze(preview);
+                Message msg = new Message();
+                msg.obj = histogram;
+                handler.sendMessage(msg);
+            } else {
+                Log.e(TAG, "updateHistogramView: bitmap is null");
+            }
         });
         th.start();
     }
@@ -108,10 +113,10 @@ public class ExifDialogViewModel extends ViewModel {
         Date photoDate;
         try {
             photoDate = ParseExif.sFormatter.parse(savedDate); //parsing with the same formatter with which it was saved
-        } catch (ParseException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return "";
         }
-        return displayedDateFormat.format(photoDate);
+        return displayedDateFormat.format(photoDate == null ? new Date() : photoDate);
     }
 }
