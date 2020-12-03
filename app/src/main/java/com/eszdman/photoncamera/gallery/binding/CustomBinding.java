e88376bff4d06b5eb320ebb142e83d1c2117ca19
Vibhor Srivastava <69432426+vibhorSrv@users.noreply.github.com>
2020-12-03 11:05:03 +0530

    Gallery grid thumbnail loading animation

--- app/src/main/java/com/eszdman/photoncamera/gallery/binding/CustomBinding.java
+++ app/src/main/java/com/eszdman/photoncamera/gallery/binding/CustomBinding.java
@@ -3,6 +3,8 @@ package com.eszdman.photoncamera.gallery.binding;
 import android.graphics.Bitmap;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import androidx.databinding.BindingAdapter;
 import com.eszdman.photoncamera.gallery.model.ExifDialogModel;
@@ -30,6 +32,27 @@ public class CustomBinding {
 
     @BindingAdapter("imageFromBitmap")
     public static void setImageBitmap(ImageView view, Bitmap bitmap) {
-        view.setImageBitmap(bitmap);
+        setBitmapWithAnimation(view, bitmap);
+    }
+
+    private static void setBitmapWithAnimation(ImageView view, Bitmap bitmap) {
+        Animation anim_out = AnimationUtils.loadAnimation(view.getContext(), android.R.anim.fade_out);
+        Animation anim_in = AnimationUtils.loadAnimation(view.getContext(), android.R.anim.fade_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.setImageBitmap(bitmap);
+                view.startAnimation(anim_in);
+            }
+        });
+        view.startAnimation(anim_out);
     }
 }
