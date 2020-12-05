99037bd55699c22bd42872622e6787669795a9c0
Vibhor Srivastava <69432426+vibhorSrv@users.noreply.github.com>
2020-12-05 13:27:25 +0530

    Gallery: Fixed Edit Intent, Share Intent

---
+++ app/src/main/java/com/eszdman/photoncamera/gallery/helper/Constants.java
@@ -0,0 +1,6 @@
+package com.eszdman.photoncamera.gallery.helper;
+
+public class Constants {
+    public static final int REQUEST_EDIT_IMAGE = 1001;
+
+}
--- app/src/main/java/com/eszdman/photoncamera/gallery/ui/fragments/ImageViewerFragment.java
+++ app/src/main/java/com/eszdman/photoncamera/gallery/ui/fragments/ImageViewerFragment.java
@@ -1,11 +1,13 @@
 package com.eszdman.photoncamera.gallery.ui.fragments;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,20 +27,26 @@ import com.eszdman.photoncamera.R;
 import com.eszdman.photoncamera.databinding.FragmentGalleryImageViewerBinding;
 import com.eszdman.photoncamera.gallery.adapters.DepthPageTransformer;
 import com.eszdman.photoncamera.gallery.adapters.ImageAdapter;
+import com.eszdman.photoncamera.gallery.helper.Constants;
 import com.eszdman.photoncamera.gallery.viewmodel.ExifDialogViewModel;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ImageViewerFragment extends Fragment {
     private static final String TAG = ImageViewerFragment.class.getSimpleName();
     private final String path = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera";
     private final File[] allFiles = new File(path).listFiles((dir, name) -> name.toUpperCase().endsWith("JPG"));
+    private File newEditedFile;
     private ExifDialogViewModel exifDialogViewModel;
     private ViewPager viewPager;
     private ImageAdapter adapter;
     private NavController navController;
     private FragmentGalleryImageViewerBinding fragmentGalleryImageViewerBinding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -98,9 +106,36 @@ public class ImageViewerFragment extends Fragment {
             Uri uri = FileProvider.getUriForFile(getContext(), getContext().getPackageName() + ".provider", new File(path + "/" + fileName));
             Intent editIntent = new Intent(Intent.ACTION_EDIT);
             editIntent.setDataAndType(uri, mediaType);
-            editIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+            String outputFilePath = file.getAbsolutePath().replace(file.getName(), generateNewFileName() + '.' + FileUtils.getExtension(fileName));
+            newEditedFile = new File(outputFilePath);
+            editIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newEditedFile));
             editIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivity(Intent.createChooser(editIntent, null));
+            Intent chooser = Intent.createChooser(editIntent, null);
+            startActivityForResult(chooser, Constants.REQUEST_EDIT_IMAGE);
+        }
+    }
+
+    private String generateNewFileName() {
+        return "IMG_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.REQUEST_EDIT_IMAGE) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                if (newEditedFile != null) {
+                    if (newEditedFile.exists() && newEditedFile.length() == 0)
+                        Log.d(TAG, "onActivityResult(" + requestCode + "," + resultCode + ")->Dummy file deleted : " + newEditedFile.delete());
+                }
+            }
+            if (resultCode == Activity.RESULT_OK) {
+                if (data != null && data.getData() != null) {
+                    String savedFilePath = data.getData().getPath();
+                    Toast.makeText(getContext(), "Saved : " + savedFilePath, Toast.LENGTH_LONG).show();
+                }
+            }
+//            Log.d(TAG, "onActivityResult(): requestCode = [" + requestCode + "], resultCode = [" + resultCode + "], data = [" + data + "]");
         }
     }
 
@@ -139,7 +174,8 @@ public class ImageViewerFragment extends Fragment {
         intent.putExtra(Intent.EXTRA_STREAM, uri);
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         intent.setType(mediaType);
-        startActivity(intent);
+        Intent chooser = Intent.createChooser(intent, null);
+        startActivity(chooser);
     }
 
     private void onExifButtonClick(View view) {
