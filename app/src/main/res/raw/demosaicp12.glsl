d5a7fd98138a3e66c2b69599eb1c2df2378c8412
Eszdman <eszdman2015@gmail.com>
2021-09-25 20:15:10 +0300

    Updated demosaicing

--- app/src/main/res/raw/demosaicp12.glsl
+++ app/src/main/res/raw/demosaicp12.glsl
@@ -16,6 +16,7 @@ uniform sampler2D GradBuffer;
 #define FUSEMPY 1.4
 #define NOISEO 0.0
 #define NOISES 0.0
+#define PI 3.1415926535897932384626433832795
 out float Output;
 float normpdf(in float x, in float sigma){return 0.39894*exp(-0.5*x*x/(sigma*sigma))/sigma;}
 void main() {
@@ -76,22 +77,34 @@ void main() {
             W = MIN/MAX;
         }
         float avr = (green[0]+green[1]+green[2]+green[3])/4.0;
-        float N = sqrt(avr*NOISES + NOISEO)/5.0;
+        //float N = sqrt(avr*NOISES + NOISEO)/5.0;
         //W=W*W;
         //W=sqrt(W);
         //float badGR = (abs(HV.r-HV.g)*5.0)/((HV.r+HV.g));
         HV/=weights;
-        if(abs(HV.r) > N || abs(HV.g) > N){
-            if (HV.g > HV.r){
-                Output = (green[1]*grad[1] + green[2]*grad[2])/(grad[1]+grad[2]);
-                //Output = (green[1] + green[2])/(2.0);
-            } else {
-                Output = (green[0]*grad[0] + green[3]*grad[3])/(grad[0]+grad[3]);
-                //Output = (green[0] + green[3])/(2.0);
-            }
+        //if(abs(HV.r) > N || abs(HV.g) > N){
+        //float angle = atan(dxy.y,dxy.x)+PI;
+        vec2 dxy2 = vec2((HV.x+HV.y)/2.0,(HV.y-HV.x)/2.0);
+        float avrg = (green[0]*grad[0]+green[1]*grad[1]+green[2]*grad[2]+green[3]*grad[3])/(grad[0]+grad[1]+grad[2]+grad[3]);
+        grad[0] = 1.0/sqrt(HV.x*HV.x+abs(green[0]-avrg)+demosw);
+        grad[1] = 1.0/sqrt(HV.y*HV.y+abs(green[1]-avrg)+demosw);
+        grad[2] = 1.0/sqrt(HV.y*HV.y+abs(green[2]-avrg)+demosw);
+        grad[3] = 1.0/sqrt(HV.x*HV.x+abs(green[3]-avrg)+demosw);
+
+        //Output = (green[0]*grad[0] +green[1]*grad[1] + green[2]*grad[2]+ green[3]*grad[3])/(grad[0]+grad[1]+grad[2]+grad[3]);
+        float outp2 = 0.0;
+
+        if (HV.y > HV.x){
+            Output = (green[1]*grad[1] + green[2]*grad[2])/(grad[1]+grad[2]);
+            //Output = (green[1] + green[2])/(2.0);
         } else {
-            Output = avr;
+            Output = (green[0]*grad[0] + green[3]*grad[3])/(grad[0]+grad[3]);
+            //Output = (green[0] + green[3])/(2.0);
         }
+        //Output = mix(outp2,Output,0.5 - 0.5*abs(HV.y-HV.x));
+        /*} else {
+            Output = avr;
+        }*/
 
 
         W = mix(FUSEMIN,FUSEMAX,clamp((W+FUSESHIFT)*FUSEMPY,0.0,1.0));
