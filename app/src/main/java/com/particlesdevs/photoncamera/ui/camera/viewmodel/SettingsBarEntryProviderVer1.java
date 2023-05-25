b4fa82d38a2cdfb4cf75a2e640480ba9c06ccf5c
Yoongunwo <glove2275@pusan.ac.kr>
2023-05-25 21:02:38 +0900

    Refactor SettingsBarEntryProviderFactory

--- app/src/main/java/com/particlesdevs/photoncamera/ui/camera/viewmodel/SettingsBarEntryProviderVer1.java
+++ app/src/main/java/com/particlesdevs/photoncamera/ui/camera/viewmodel/SettingsBarEntryProviderVer1.java
@@ -3,5 +3,7 @@ package com.particlesdevs.photoncamera.ui.camera.viewmodel;
 import com.particlesdevs.photoncamera.ui.camera.viewmodel.SettingsBarEntryProvider;
 
 public class SettingsBarEntryProviderVer1 extends SettingsBarEntryProvider {
-
+    public SettingsBarEntryProviderVer1(){
+        super();
+    }
 }
