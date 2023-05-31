105c2117b7a6609a8c95f90bce5a5becf927919a
Yoongunwo <glove2275@pusan.ac.kr>
2023-05-31 09:26:08 +0900

    Fix : SettingsBarEntryProvider
    SettingsBarEntryProvider 클래스를 사용하는 부분을
    SettingsBarEntryProviderFactory 클래스 적용

--- app/src/main/java/com/particlesdevs/photoncamera/ui/camera/viewmodel/SettingsBarEntryProviderFactory.java
+++ app/src/main/java/com/particlesdevs/photoncamera/ui/camera/viewmodel/SettingsBarEntryProviderFactory.java
@@ -1,11 +1,16 @@
 package com.particlesdevs.photoncamera.ui.camera.viewmodel;
 
+import androidx.lifecycle.ViewModelProvider;
+
+import com.particlesdevs.photoncamera.ui.camera.CameraFragment;
+
 public class SettingsBarEntryProviderFactory{
-    public static SettingsBarEntryProvider SettingsBarEntryProver(SettingsBarEntryProviderID settingsBarEntryProviderID){
+    public SettingsBarEntryProviderFactory(){}
+    public SettingsBarEntryProvider getSettingsBarEntryProvider(CameraFragment cameraFragment, SettingsBarEntryProviderID settingsBarEntryProviderID){
         SettingsBarEntryProvider settingsBarEntryProvider = null;
 
         switch (settingsBarEntryProviderID){
-            case V1 : settingsBarEntryProvider = new SettingsBarEntryProviderVer1(); break;
+            case V1 : settingsBarEntryProvider = new ViewModelProvider(cameraFragment).get(SettingsBarEntryProviderVer1.class); break;
             case V2 : break;
         }
         return settingsBarEntryProvider;
